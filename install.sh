30aec4157853bd16d3da82b2b8361c3755995ebd
kajsf <asasf@asf>
2019-03-29 19:46:01 +0100

    install angepasst

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-# small install script for this GripperViewContainer. Will probably not work 100% all the time, have fun debugging!
+# small install script for this GripperViewContainer. Will probably not work all the time
 
 # needed: python(2) and python3, with pip and pip3
 pip install -r requirements.txt
 pip3 install -r requirements.txt
+pip install perception_exactly_this_is_somehow_required/
+#irgendwie installiert der bei mir numpy immer doppelt...
+pip uninstall numpy 
 cd gqcnn_jeffbranch_adapted && python setup.py install && cd ..
+
+#viel erfolg
